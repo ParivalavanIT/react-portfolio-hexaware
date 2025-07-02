c0cafba7b1620e6615cca24b2aa929f3ae8c00cf
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2025-07-02 10:47:29 +0530

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EmptyPage from './pages/EmptyPage';
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/empty" element={<EmptyPage/>}/>
+        <Route path="/empty" element={<EmptyPage />} />
       </Routes>
     </Router>
   );
